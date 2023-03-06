282b0c4bc129d3062b2a7f4e5c9240c20ffae1ee
odanetpro <odanetpro@gmail.com>
2023-03-06 21:03:38 +0900

    feat: user can create an answer on question page

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,18 +5,14 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
   let(:answer) { question.create(:answer) }
+  let(:user) { create(:user) }
+
+  before { login(user) }
 
   describe 'GET #show' do
     it 'renders show view' do
       get :show, params: { id: question }
       expect(response).to render_template :show
-    end
-  end
-
-  describe 'GET #new' do
-    it 'renders new view' do
-      get :new, params: { question_id: question }
-      expect(response).to render_template :new
     end
   end
 
@@ -28,9 +24,9 @@ RSpec.describe AnswersController, type: :controller do
         end.to change(Answer, :count).by(1)
       end
 
-      it 'redirects to show view' do
+      it 'redirects to question show view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        expect(response).to redirect_to assigns(:answer)
+        expect(response).to redirect_to question
       end
     end
 
@@ -41,9 +37,9 @@ RSpec.describe AnswersController, type: :controller do
         end.to_not change(Answer, :count)
       end
 
-      it 're-render new view' do
+      it 're-render question show view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
-        expect(response).to render_template :new
+        expect(response).to render_template 'questions/show'
       end
     end
   end
---
+++ spec/features/answer/create_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can create an answer to the question', "
+  In order to help other users find a solution to the problem
+  As an authenticated user
+  I'd like to be able to answer the question on the question page
+" do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'answers the question' do
+      fill_in 'answer[body]', with: 'Test answer'
+      click_button 'Post Your Answer'
+
+      expect(page).to have_content 'Your answer successfully created!'
+      # expect(page).to have_content question.title
+      # expect(page).to have_content 'Test answer'
+    end
+
+    scenario 'answers the question with errors' do
+      click_button 'Post Your Answer'
+
+      expect(page).to have_content "Body can't be blank"
+    end
+  end
+
+  scenario 'Unauthenticated user tries answer the question' do
+    visit question_path(question)
+    click_button 'Post Your Answer'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
