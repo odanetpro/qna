ab22e630ad3e3a6b624c1540eb5e306b96e40a66
odanetpro <odanetpro@gmail.com>
2023-03-09 12:45:13 +0900

    feat: user can view question and answers to it

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence :body do |n|
+    "AnswerText#{n}"
+  end
+
   factory :answer do
-    body { 'MyText' }
+    body
     question { nil }
 
     trait :invalid do
