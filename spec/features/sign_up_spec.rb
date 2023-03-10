69c6dc9893b777ee3ecfa78714449fecf142e730
odanetpro <odanetpro@gmail.com>
2023-03-10 10:03:42 +0900

    feat: user can sign up

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User can sign up', "
+  In order to be able to authenticate
+  As an unauthenticated user
+  I'd like to be able to register
+" do
+  given(:user) { create(:user) }
+
+  describe 'Unauthenticated user' do
+    background { visit new_user_registration_path }
+
+    scenario 'tries to sign up' do
+      fill_in 'Email', with: 'user@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+
+      click_button 'Sign up'
+
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+
+    scenario 'tries to sign up with blank parameters' do
+      click_button 'Sign up'
+
+      expect(page).to have_content "Email can't be blank"
+      expect(page).to have_content "Password can't be blank"
+    end
+
+    scenario 'tries to sign up with with a non-unique email' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      fill_in 'Password confirmation', with: user.password
+
+      click_button 'Sign up'
+
+      expect(page).to have_content 'Email has already been taken'
+    end
+
+    scenario 'tries to sign up with a mismatched password and confirmation' do
+      fill_in 'Email', with: 'user@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '123456789'
+
+      click_button 'Sign up'
+
+      expect(page).to have_content "Password confirmation doesn't match Password"
+    end
+  end
+
+  scenario 'Authenticated user tries to sign up' do
+    sign_in(user)
+    visit new_user_registration_path
+
+    expect(page).to have_content 'You are already signed in.'
+  end
+end
