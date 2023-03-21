2538646441eb7960fb91a96fa312be5237587caf
odanetpro <odanetpro@gmail.com>
2023-03-21 11:51:21 +0900

    feat: user creates question with many files

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,12 +43,12 @@ class QuestionsController < ApplicationController
   private
 
   def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+    @question ||= params[:id] ? Question.with_attached_files.find(params[:id]) : Question.new
   end
 
   helper_method :question
 
   def question_params
-    params.require(:question).permit(:title, :body, :file).merge(author_id: current_user.id)
+    params.require(:question).permit(:title, :body, files: []).merge(author_id: current_user.id)
   end
 end
