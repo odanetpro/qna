739d47e48da08550949f3f20bba42b1447880ec8
odanetpro <odanetpro@gmail.com>
2023-03-26 18:21:53 +0900

    fix: fixed disabled file input when direct upload

--- app/javascript/utilities/answers.js
+++ app/javascript/utilities/answers.js
@@ -8,5 +8,8 @@ $(document).on('turbolinks:load', function() {
     $('.answer-' + answerId).children('.delete-answer-link').addClass('hidden')
     $('.answer-' + answerId).children('.best-answer-link').addClass('hidden')
     $('form#edit-answer-' + answerId).removeClass('hidden')
+
+    $('form#edit-answer-' + answerId + ' #answer_files').prop('disabled', false)
+    $('form#edit-answer-' + answerId + ' #answer_files').val('')
   })
 })
--- app/javascript/utilities/questions.js
+++ app/javascript/utilities/questions.js
@@ -5,5 +5,8 @@ $(document).on('turbolinks:load', function() {
     $(this).addClass('hidden')
     $('.delete-question-link').addClass('hidden')
     $('form#edit-question').removeClass('hidden')
+
+    $('form#edit-question #question_files').prop('disabled', false)
+    $('form#edit-question #question_files').val('')
   })
 })
