489d741a924ef79a839f47636927212b7c353c29
odanetpro <odanetpro@gmail.com>
2023-03-28 11:50:23 +0900

    feat: user can add links to answer

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Link < ApplicationRecord
-  belongs_to :question
+  belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@
 
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  has_many :links, dependent: :destroy
+  has_many :links, dependent: :destroy, as: :linkable
 
   has_many_attached :files
 
