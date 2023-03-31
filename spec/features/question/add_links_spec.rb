a9f796d72a578d33d6501049638880ea16c6b71c
odanetpro <odanetpro@gmail.com>
2023-03-31 19:07:30 +0900

    feat: user can add links when edit question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -12,71 +12,106 @@ feature 'User can add links to question', "
   given(:google_url) { 'http://google.com' }
   given(:yandex_url) { 'http://yandex.ru' }
 
-  before do
-    sign_in(user)
-    visit new_question_path
-  end
+  describe 'CREATE' do
+    before do
+      sign_in(user)
+      visit new_question_path
+    end
 
-  scenario 'User adds link when asks question' do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+    scenario 'User adds link when asks question' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
 
-    fill_in 'Link name', with: 'Google'
-    fill_in 'Url', with: google_url
-
-    click_on 'Ask'
-
-    expect(page).to have_link 'Google', href: google_url
-  end
-
-  scenario 'User adds links when asks question', js: true do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
-
-    click_on 'add link'
-
-    nested_fields = all('.nested-fields')
-
-    within(nested_fields.first) do
       fill_in 'Link name', with: 'Google'
       fill_in 'Url', with: google_url
+
+      click_on 'Ask'
+
+      expect(page).to have_link 'Google', href: google_url
     end
 
-    within(nested_fields.last) do
-      fill_in 'Link name', with: 'Yandex'
-      fill_in 'Url', with: yandex_url
+    scenario 'User adds links when asks question', js: true do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+
+      click_on 'add link'
+
+      nested_fields = all('.nested-fields')
+
+      within(nested_fields.first) do
+        fill_in 'Link name', with: 'Google'
+        fill_in 'Url', with: google_url
+      end
+
+      within(nested_fields.last) do
+        fill_in 'Link name', with: 'Yandex'
+        fill_in 'Url', with: yandex_url
+      end
+
+      click_on 'Ask'
+
+      expect(page).to have_link 'Google', href: google_url
+      expect(page).to have_link 'Yandex', href: yandex_url
     end
 
-    click_on 'Ask'
+    scenario 'User adds wrong link when asks question' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
 
-    expect(page).to have_link 'Google', href: google_url
-    expect(page).to have_link 'Yandex', href: yandex_url
+      fill_in 'Link name', with: 'Wrong link'
+      fill_in 'Url', with: 'yandex'
+
+      click_on 'Ask'
+
+      expect(page).to_not have_link 'Wrong link', href: 'yandex'
+      expect(page).to have_content 'url is invalid'
+    end
+
+    scenario 'User adds gist link when asks question', js: true do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+
+      fill_in 'Link name', with: 'Gist'
+      fill_in 'Url', with: gist_url
+
+      click_on 'Ask'
+
+      within('.question .gist-content') do
+        expect(page).to have_content "puts 'Hello, world!'"
+      end
+    end
   end
 
-  scenario 'User adds wrong link when asks question' do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+  describe 'EDIT' do
+    scenario 'User adds links when edit question', js: true do
+      sign_in(user)
 
-    fill_in 'Link name', with: 'Wrong link'
-    fill_in 'Url', with: 'yandex'
+      question = create(:question, author: user)
+      visit question_path(question)
 
-    click_on 'Ask'
+      within '.question' do
+        click_on 'Edit'
 
-    expect(page).to_not have_link 'Wrong link', href: 'yandex'
-    expect(page).to have_content 'url is invalid'
-  end
+        click_on 'add link'
+        click_on 'add link'
 
-  scenario 'User adds gist link when asks question', js: true do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+        nested_fields = all('.nested-fields')
 
-    fill_in 'Link name', with: 'Gist'
-    fill_in 'Url', with: gist_url
+        within(nested_fields.first) do
+          fill_in 'Link name', with: 'Google'
+          fill_in 'Url', with: google_url
+        end
 
-    click_on 'Ask'
+        within(nested_fields.last) do
+          fill_in 'Link name', with: 'Yandex'
+          fill_in 'Url', with: yandex_url
+        end
 
-    within('.question .gist-content') do
-      expect(page).to have_content "puts 'Hello, world!'"
+        click_on 'Save'
+
+        expect(page).to have_link 'Google', href: google_url
+        expect(page).to have_link 'Yandex', href: yandex_url
+      end
     end
   end
 end
