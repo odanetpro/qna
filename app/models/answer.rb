e8c5ad999f4b9723b9ebefd5e98fb39efbac6b06
odanetpro <odanetpro@gmail.com>
2023-04-04 19:00:20 +0900

    feat: add concern linkable

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
-  has_many :links, dependent: :destroy, as: :linkable
+  include Linkable
 
   belongs_to :question
   belongs_to :author, class_name: 'User'
 
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  include Linkable
+
   has_many :answers, dependent: :destroy
-  has_many :links, dependent: :destroy, as: :linkable
   has_one :award, dependent: :destroy
 
   has_many_attached :files
@@ -10,7 +11,6 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :best_answer, class_name: 'Answer', optional: true
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
   accepts_nested_attributes_for :award, reject_if: :all_blank
 
   validates :title, :body, presence: true
