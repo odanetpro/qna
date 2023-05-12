1b7894540d1dcf76785e4d0127dd52d84838084b
odanetpro <odanetpro@gmail.com>
2023-05-12 12:48:44 +0900

    feat: added api v1 profiles index

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,6 +3,11 @@
 module Api
   module V1
     class ProfilesController < BaseController
+      def index
+        authorize! :read, User
+        render json: User.where.not(id: current_resource_owner.id)
+      end
+
       def me
         authorize! :me, current_resource_owner
         render json: current_resource_owner
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -26,13 +26,37 @@ describe 'Profiles API', type: :request do
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
-          expect(json).to_not have_key(attr)
+          expect(json['user']).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+      let(:api_path) { '/api/v1/profiles' }
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let(:access_token) { create(:access_token, resource_owner_id: users.first.id) }
+
+      before { get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns list of users' do
+        expect(json['users'].size).to eq 2
+      end
+
+      it 'returns list of users except current user' do
+        json['users'].each do |user|
+          expect(user['id']).to_not eq users.first.id
         end
       end
     end
