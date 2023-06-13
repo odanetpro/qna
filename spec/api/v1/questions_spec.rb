9d64d4d5ccca5425ecee5aa8c4a41ac327ca49f7
odanetpro <odanetpro@gmail.com>
2023-06-13 22:36:12 +0900

    fix: api/v1/question test

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -17,7 +17,7 @@ describe 'Questions API', type: :request do
       let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
       let(:question) { questions.first }
-      let(:question_response) { json['questions'].first }
+      let(:question_response) { json['questions'].last }
       let!(:answers) { create_list(:answer, 3, question: question) }
 
       before { get '/api/v1/questions', params: { access_token: access_token.token }, headers: headers }
