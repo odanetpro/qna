4b0fbdcc559397931a8cbf3d4729e0eb852d5b1d
odanetpro <odanetpro@gmail.com>
2023-06-13 22:36:30 +0900

    fix: daily digest test

--- spec/mailers/daily_digest_spec.rb
+++ spec/mailers/daily_digest_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe DailyDigestMailer, type: :mailer do
   describe 'digest' do
     let(:user) { create(:user) }
     let(:mail) { DailyDigestMailer.digest(user) }
+    let!(:questions) { create_list(:question, 2) }
 
     it 'renders the headers' do
       expect(mail.subject).to eq('Daily Digest from QnA')
@@ -14,8 +15,6 @@ RSpec.describe DailyDigestMailer, type: :mailer do
     end
 
     it 'renders the body' do
-      questions = create_list(:question, 2)
-
       questions.each do |q|
         expect(mail.body.encoded).to match(q.title)
       end
